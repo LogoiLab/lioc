ee958a5d6664afef4d1e2823e90d9eb21a48c8e6
Chad Baxter <cbaxter@mail.umw.edu>
2020-05-31 22:38:38 -0400

    switched to easy2

--- src/interaction.rs
+++ src/interaction.rs
@@ -1,18 +1,22 @@
 use crypto_hash::{Algorithm, hex_digest};
 
-pub fn follow_url(url: String) -> String {
-    let mut data = Vec::new();
-    let mut curl = curl::easy::Easy::new();
-    curl.url(url.as_str()).unwrap();
-    curl.follow_location(true).unwrap();
-    {
-        let mut transfer = curl.transfer();
-        transfer.write_function(|new_data| {
-            data.extend_from_slice(new_data);
-            Ok(new_data.len())
-        }).unwrap();
-        transfer.perform().unwrap();
-    }
+use curl::easy::{Easy2, Handler, WriteError};
 
-    return hex_digest(Algorithm::SHA256, data.as_slice());
+struct Collector(Vec<u8>);
+
+impl Handler for Collector {
+    fn write(&mut self, data: &[u8]) -> Result<usize, WriteError> {
+        self.0.extend_from_slice(data);
+        Ok(data.len())
+    }
+}
+
+pub fn follow_url(url: String) -> String {
+    let mut easy = Easy2::new(Collector(Vec::new()));
+    easy.get(true).unwrap();
+    easy.url(url.as_str()).unwrap();
+    easy.follow_location(true).unwrap();
+    easy.perform().unwrap();
+
+    return hex_digest(Algorithm::SHA256, &easy.get_ref().0);
 }
